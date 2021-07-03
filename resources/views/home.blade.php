779693c7a0dbeaf8a5dece708cabe6847bb92662
danilo de souza lima <danilo.lima@oi.net.br>
2021-07-03 20:30:02 -0300

    'ajustes'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,40 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header bg-primary text-white">
-                    Cadastro de Fornecedor
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header bg-primary text-white">
+                        Cadastro de Fornecedor
+                    </div>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        You are logged in!
+                    </div>
                 </div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    Oiiii
-                </div>
-            </div>
-        </div>
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header bg-primary text-white">
-                    Lista de Fornecedores Cadastrados
-                </div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    Oiiii
+                <br><br>
+                <div class="card">
+                    <div class="card-header bg-primary text-white">
+                        Lista de Fornecedores Cadastrados
+                    </div>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        You are logged in!
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
